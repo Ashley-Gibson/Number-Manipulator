49ee537c708f2d3c3173fe2c8352eed9d3a098ff
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-03-11 13:24:26 +0000

    task/NM-104 - Initial Setup of Unit Testing Suite

---
+++ Source Code/Unit Testing/Logic.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Testing
+{
+    [TestClass]
+    public class Logic_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Source Code/Unit Testing/UI.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Testing
+{
+    [TestClass]
+    public class UI_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
